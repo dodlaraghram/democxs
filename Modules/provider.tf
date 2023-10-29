1ba5a1a6d3f5f094db04b930073ed14600ff2902
kirand240222 <DRaghuram@carnival.com>
2023-10-29 19:38:52 +0530

    added config

--- Modules/provider.tf
+++ Modules/provider.tf
@@ -13,6 +13,6 @@ provider "azurerm" {
   subscription_id = "ae231b2f-8211-4f88-b5c1-df0bf3fd97f0"
   tenant_id = "ae58859f-9c48-4516-b237-63f152186d8d"
   client_id = "402e1460-1764-4db2-939f-e13e7ed9447b"
-  client_secret = "nKc8Q~D3ZxkBFInuakrg4.KX03hVZQYtCb4_hc5F"
+  client_secret = "nCh8Q~kvPHPJjq9CKvEJMYr_3vg5ypnLO~CFqalw"
   features{}
 }
